418cf2c56606c420d76957b9c52c185f62761e66
Sham <shamshyrak.zholdasbek@gmail.com>
2026-08-12 19:27:08 +0500

    refactor: update Display component props to use optional chaining and default values

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -10,12 +10,16 @@ function formatOperand(operand: string | number | null) {
 }
 
 type DisplayType = {
-  currentOperand: string | number | null;
-  previousOperand: string | number | null;
-  operation: string | null;
+  currentOperand?: string | number | null;
+  previousOperand?: string | number | null;
+  operation?: string | null;
 };
 
-function Display({ currentOperand, previousOperand, operation }: DisplayType) {
+function Display({
+  currentOperand = "0",
+  previousOperand = null,
+  operation = null,
+}: DisplayType) {
   return (
     <div className="text-right flex flex-col mx-10 mt-10 rounded-md border border-input h-1/3 p-10 justify-center">
       <div className="text-8xl ">
@@ -27,11 +31,5 @@ function Display({ currentOperand, previousOperand, operation }: DisplayType) {
     </div>
   );
 }
-
-Display.defaultProps = {
-  currentOperand: "0",
-  previousOperand: null,
-  operation: null,
-};
 
 export default Display;
