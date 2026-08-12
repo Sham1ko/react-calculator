bb2fece3ab96751d01d3f9a3e1f3e88608ec97a6
Sham <shamshyrak.zholdasbek@gmail.com>
2026-08-13 00:36:55 +0500

    fix: adjust margin and max-width properties for improved responsiveness in GithubButton

--- src/components/GithubButton/index.tsx
+++ src/components/GithubButton/index.tsx
@@ -10,7 +10,7 @@ const GithubButton = () => {
             className="group flex items-center overflow-hidden rounded-full bg-slate-200 p-2 dark:bg-slate-700"
         >
             <Github size={20} className="shrink-0" />
-            <span className="max-w-0 overflow-hidden whitespace-nowrap text-sm font-medium transition-all duration-300 delay-300 group-hover:ml-2 group-hover:max-w-[6rem] group-hover:delay-0">
+            <span className="ml-2 overflow-hidden whitespace-nowrap text-sm font-medium lg:ml-0 lg:max-w-0 lg:transition-all lg:duration-300 lg:delay-300 lg:group-hover:ml-2 lg:group-hover:max-w-[6rem] lg:group-hover:delay-0">
                 sham1ko
             </span>
         </a>
